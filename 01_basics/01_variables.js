246afdf6dbc17a31a5803a8527457a2872f73625
podder-sujit <140741554+podder-sujit@users.noreply.github.com>
2023-07-28 20:15:38 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 457
+let accountEmail = "sujit33podder@gmail.com"
+var accountPassword = "12345"
+accountCity = "bamangachhi"
+let accountState;
+
+// accountId = 5 //not allowed
+
+accountEmail = "sujit1@gmail.com"
+accountPassword = "597"
+accountCity ="barasat"
+
+console.log(accountId);
+
+/* prefer not to use var because of issue in block scope and functional scope */
+
+console.table([accountId, accountPassword, accountCity, accountEmail, accountState]);
